a377465df941a924e7c6c0689557973b501c0771
akarafat99 <arafatabdulkhaled@gmail.com>
2025-05-03 04:45:32 +0600

    1

--- admin/reset-pass-3.php
+++ admin/reset-pass-3.php
@@ -75,7 +75,7 @@ if (isset($_POST['resetPassword'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
 
-    <title>JUST Hall</title>
+    <title>MM Hall</title>
 
 
 </head>
